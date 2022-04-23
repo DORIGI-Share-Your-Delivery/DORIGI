54de21a6742aa1c1b01df2cd1617608ec3435490
Ryokuman <ryokuman@naver.com>
2022-04-23 16:34:20 +0900

    backup

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
-import "../App.css";
+import "../Assets/styles/Login.css"
+import InputUnstyled from '@mui/base/InputUnstyled';
 
 
 export default function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='susususususu'>
+            <div className='picture'>
+
+            </div>
+            <div className='loginPage'>
+                <div className='title'>
+                    <span style={{ fontFamily:'SpoqaBold' }}>DORIGI</span>
+                    <span style={{ fontFamily:'SpoqaThin' , marginLeft:"10px" }}>Sign In</span>
+                </div>
+                <div className='login_form'>
+                    <form>
+                        <InputUnstyled />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
