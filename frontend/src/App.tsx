7924e83a233b0fb745fcb7b8bc7c9b2c6a5fdebe
hyeontack5 <sksgusxor@gmail.com>
2022-05-13 12:59:46 +0900

    MOD: App.tsx { Error Modified }

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,46 +18,44 @@ const theme = createTheme();
 
 //section data type -> interface
 interface section {
-    title: string;
-    url: string;
+  title: string;
+  url: string;
 }
 
 const sections: section[] = [
-    { title: "게시판", url: "/board" },
-    { title: "채팅", url: "/chat" },
-    { title: "맛집지도", url: "/restaurant" },
+  { title: "게시판", url: "/board" },
+  { title: "채팅", url: "/chat" },
+  { title: "맛집지도", url: "/restaurant" },
 ];
 
 function App() {
-    const test: string | null = null;
-
-    return (
-        <Router>
-            <div className="App">
-                <ThemeProvider theme={theme}>
-                    <Header sections={sections} />
-                    <Routes>
-                        <Route path="/*" element={<ErrorPage />} />
-                        <Route path="/" element={<Main />} />
-                        <Route path="/board" element={<Board />} />
-                        <Route path="/chat" element={<Chatting />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/restaurant" element={<Restaurant />} />
-                        <Route path="/search" element={<Search />}>
-                            <Route
-                                path="/search/:selectedValue/:searchValue"
-                                element={<Search />}
-                            />
-                        </Route>
-                    </Routes>
-                    <Footer
-                        title={"title"}
-                        description={"@DORIGI, Inc. 2022. 2022-DORIGI-TeamI"}
-                    />
-                </ThemeProvider>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Header sections={sections} />
+          <Routes>
+            <Route path="/*" element={<ErrorPage />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/chat" element={<Chatting />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/restaurant" element={<Restaurant />} />
+            <Route path="/search" element={<Search />}>
+              <Route
+                path="/search/:selectedValue/:searchValue"
+                element={<Search />}
+              />
+            </Route>
+          </Routes>
+          <Footer
+            title={"title"}
+            description={"@DORIGI, Inc. 2022. 2022-DORIGI-TeamI"}
+          />
+        </ThemeProvider>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
