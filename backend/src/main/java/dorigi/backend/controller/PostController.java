e31f5acb40b244929cab0674af0d583ad5693e94
YujeongLEE <dldbwjd24@ewhain.net>
2022-06-08 08:24:14 -0700

    feat : 게시판 조회(parm: page)

--- backend/src/main/java/dorigi/backend/controller/PostController.java
+++ backend/src/main/java/dorigi/backend/controller/PostController.java
@@ -1,10 +1,15 @@
 package dorigi.backend.controller;
 
 import dorigi.backend.domain.BoardsInfo;
+import dorigi.backend.domain.Post;
 import dorigi.backend.domain.UserInfo;
 import dorigi.backend.service.BoardsService;
 import dorigi.backend.service.UserService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,8 +24,11 @@ public class PostController {
     }
 
     @GetMapping("/boards") // 상위 랭킹 정보를 가져옴
-    public List<BoardsInfo> getAllPosts(){
-        return boardsService.findAllPosts();
+    public List<Post> getAllPosts(@RequestParam(value="pageNum", required = false, defaultValue = "1")Integer pageNum){
+
+        Pageable paging = PageRequest.of(pageNum-1,15, Sort.by("regDate")); //page 0부터임!
+
+        return boardsService.findPosts(paging);
     }
 
 }
--- backend/src/main/java/dorigi/backend/service/BoardsService.java
+++ backend/src/main/java/dorigi/backend/service/BoardsService.java
@@ -1,24 +1,44 @@
 package dorigi.backend.service;
 
 import dorigi.backend.domain.BoardsInfo;
+import dorigi.backend.domain.Post;
 import dorigi.backend.domain.UserInfo;
 import dorigi.backend.repository.BoardsRepository;
 import dorigi.backend.repository.UserRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BoardsService {
 
-    public BoardsService(BoardsRepository boardsRepository) {
+    private final BoardsRepository boardsRepository;
+    private final UserRepository userRepository;
+
+    public BoardsService(BoardsRepository boardsRepository, UserRepository userRepository) {
         this.boardsRepository = boardsRepository;
+        this.userRepository = userRepository;
     }
 
-    private final BoardsRepository boardsRepository;
 
-    public List<BoardsInfo> findAllPosts() {
-        return boardsRepository.findAll();
+    //전체조회
+    public List<BoardsInfo> findAllPosts(Pageable pageable) {
+        return boardsRepository.findAll(pageable).getContent();
+    }
+
+    //전체 조회후 프론트에서 요청한 형식으로 내보내기
+    public List<Post> findPosts(Pageable pageable){
+        List<BoardsInfo> posts = findAllPosts(pageable);//전체조회
+        List<Post> res = new ArrayList<>();
+
+        for (BoardsInfo b : posts) {
+            String user_nick = userRepository.findByUserId(b.getUserId()).getNickname();//닉네임 찾기
+            Post p = new Post(b.getBoardId(),b.getTitle(),user_nick,b.getRegDate());
+            res.add(p);
+        }
+        return res;
     }
 
 }
